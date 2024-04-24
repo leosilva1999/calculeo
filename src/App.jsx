b72187a6146341a9f336111d1ddfe69edc1e636e
Leonardo Pereira <leopereirasilva86@gmail.com>
2024-04-24 17:42:10 -0300

    project colors tested and buttons created

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,27 @@ function App() {
 
   return ( 
       <div className='calculator'>
-        <button className='number'>7</button>
-        <button className='number'>8</button>
-        <button className='number'>9</button><p />
-        <button className='number'>4</button>
-        <button className='number'>5</button>
-        <button className='number'>6</button><p />
-        <button className='number'>1</button>
-        <button className='number'>2</button>
-        <button className='number'>3</button>
-
+        <div className='display'>
+          <p><h1>000</h1></p>
+        </div>
+        <div className='buttonNumber'>
+          <button>AC</button>
+          <button>*</button>
+          <button>+-</button>
+          <button>%</button><p />
+          <button className='number'>7</button>
+          <button className='number'>8</button>
+          <button className='number'>9</button>
+          <button>/</button><p />
+          <button className='number'>4</button>
+          <button className='number'>5</button>
+          <button className='number'>6</button>
+          <button>X</button><p />
+          <button className='number'>1</button>
+          <button className='number'>2</button>
+          <button className='number'>3</button>
+          <button>-</button><p />
+        </div>
       </div>
   )
 }
